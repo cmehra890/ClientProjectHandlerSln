61debdb64e3f83dcc90e334e0bc3329df1ae205c
cmehra890 <69719424+cmehra890@users.noreply.github.com>
2024-09-01 17:50:49 +0530

    Delete ClientProjectHandle_Web directory

--- ClientProjectHandle_Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ClientProjectHandle_Web.Models
-{
-	public class ErrorViewModel
-	{
-		public string? RequestId { get; set; }
-
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-	}
-}
